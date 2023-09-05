599f87b923a96e45ca8a3fe6b88c1c51b0ff13a9
DoroNyong <pride5623@naver.com>
2023-09-05 20:02:04 +0900

    [Add] 캐릭터 이동 추가, 에임 추가, 캐릭터와 맵 콜리더 설정

---
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownCharacterController _controller;
+
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    public int speed = 10;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovment(_movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void ApplyMovment(Vector2 direction)
+    {
+        direction = direction * speed;
+
+        _rigidbody.velocity = direction;
+    }
+}
