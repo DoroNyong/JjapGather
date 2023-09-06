eefb1463281defed234d390a1ae9460f56d9b36b
DoroNyong <pride5623@naver.com>
2023-09-06 19:01:11 +0900

    [Add] 시작할 시 이름 넣기, 이름 변경하기 추가

---
+++ Assets/Scripts/PanelScripts.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelScripts : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Text PanelName;
+    [SerializeField] private Text GameName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetOnPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void SetOffPanel()
+    {
+        GameName.text = PanelName.text;
+        panel.SetActive(false);
+    }
+}
